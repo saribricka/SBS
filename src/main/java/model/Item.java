e6b7617066ecdd1260d46d71312d181dc2eec20f
saribricka <saribricka44@gmail.com>
2022-01-05 00:38:34 +0100

    javadoc to test profile

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -4,18 +4,46 @@ import java.util.Date;
 
 public interface Item {
 
+	/**
+	 * Get barcode of selected item.
+	 * @return a string with the barcode.
+	 */
 	public String getBarcode();
 	
+	/**
+	 * Get name of selected item.
+	 * @return a string with the name.
+	 */
 	public String getProductName();
 	
+	/**
+	 * Get quantity of selected item.
+	 * @return an int with the quantity.
+	 */
 	public int getQuantity();
 	
+	/**
+	 * Get unit price of selected item.
+	 * @return a double with price per unit.
+	 */
 	public double getUnitPrice();
 	
+	/**
+	 * Get received date of selected item.
+	 * @return Date
+	 */
 	Date getReceivedDate();
 	
+	/**
+	 * Get expiration date of selected item.
+	 * @return Date
+	 */
 	Date getExpirationDate();
 	
+	/**
+	 * Get category of selected item.
+	 * @return category from enum ItemCategory.
+	 */
 	ItemCategory getCategory();
 	
 }
